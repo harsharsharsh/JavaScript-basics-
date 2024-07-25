43daa138e4e67c6c152f5a54ac2e4d74f62222f4
harsharsharsh <149872208+harsharsharsh@users.noreply.github.com>
2024-07-25 10:06:55 +0000

    methods done

---
+++ basic_javasript/array_methods.js
@@ -0,0 +1,15 @@
+const arr = [1,2,3,4,5,6,7,]
+console.log(arr)
+// arr.push(8) // adding var to array
+// arr.push(9)
+// arr.push("ahrsh")
+// console.log(arr)
+// arr.pop() //removeing last added element
+//console.log(arr)
+arr.unshift(true)
+console.log(arr)
+
+arr.shift()
+
+console.log(arr)
+console.log(arr.includes(25))
